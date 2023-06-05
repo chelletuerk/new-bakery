ddce90ba78469ba79ef6be4554b5d67dd4b1867a
commandNotFound <tuerkbot@talkingRobot.local>
2023-06-05 09:01:45 -0500

    rendering item name in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 
-const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
+const Cart = ({ cart }) => {
   const classes = useStyles();
 
-  const handleEmptyCart = () => onEmptyCart();
+  // const handleEmptyCart = () => onEmptyCart();
 
   const renderEmptyCart = () => (
     <Typography variant='subtitle1'>You have no items in your shopping cart,
@@ -20,15 +20,16 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
   const renderCart = () => (
     <>
       <Grid container spacing={3}>
-        {cart.line_items.map((lineItem) => (
-          <Grid item xs={12} sm={4} key={lineItem.id}>
+        {cart.line_items.map(item => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <div>{item.name}</div>
           </Grid>
         ))}
       </Grid>
       <div className={classes.cardDetails}>
         <Typography variant='h4'>Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
         <div>
-          <Button className={classes.emptyButton} size='large' type='button' variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
+          <Button className={classes.emptyButton} size='large' type='button' variant="contained" color="secondary">Empty cart</Button>
           <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
         </div>
       </div>
