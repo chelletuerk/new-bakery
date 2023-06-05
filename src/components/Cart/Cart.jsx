029c2c05148c6ec257cf0038ecb95d1b5efad8b6
commandNotFound <tuerkbot@talkingRobot.local>
2023-06-05 09:24:06 -0500

    rendering image in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Container, Typography, Button, Grid } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
-import useStyles from './styles';
+import useStyles from './styles'
+import CartItem from './CartItem/CartItem'
 
 const Cart = ({ cart }) => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   // const handleEmptyCart = () => onEmptyCart();
 
@@ -15,22 +16,22 @@ const Cart = ({ cart }) => {
     </Typography>
   );
 
-  if (!cart.line_items) return 'Loading';
+  if (!cart.line_items) return 'Loading...'
 
   const renderCart = () => (
     <>
       <Grid container spacing={3}>
         {cart.line_items.map(item => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CartItem item={item}/>
           </Grid>
         ))}
       </Grid>
       <div className={classes.cardDetails}>
         <Typography variant='h4'>Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
         <div>
-          <Button className={classes.emptyButton} size='large' type='button' variant="contained" color="secondary">Empty cart</Button>
-          <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+          <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty cart</Button>
+          <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
         </div>
       </div>
     </>
@@ -39,7 +40,7 @@ const Cart = ({ cart }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
+      <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
       { !cart.line_items.length ? renderEmptyCart() : renderCart() }
     </Container>
   );
